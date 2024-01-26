b0e7c475b937c0847680e07522819977c8fb40e0
915dbfl <dbfl00915@naver.com>
2024-01-26 17:04:16 +0900

    [FEAT] Feed viewModel에서 사용할 input, output interface 정의

---
+++ app/src/main/java/kr/co/fastcampus/part4plus/movieapp/features/feed/presentation/output/FeedState.kt
@@ -0,0 +1,12 @@
+package kr.co.fastcampus.part4plus.movieapp.features.feed.presentation.output
+
+sealed class FeedState {
+    object Loading: FeedState()
+    class Main(
+        val movieList = List<MovieFeedItemEntity>
+    ): FeedState()
+
+    class Failed(
+        val reason: String
+    ): FeedState()
+}
