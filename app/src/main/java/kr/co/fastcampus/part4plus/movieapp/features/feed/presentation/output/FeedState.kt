1713cbeee20c1911e6a65fc47a014903c9c47e1d
915dbfl <dbfl00915@naver.com>
2024-01-27 16:02:49 +0900

    [FIX] FeedState의 movieList 파라미터 오류 해결

--- app/src/main/java/kr/co/fastcampus/part4plus/movieapp/features/feed/presentation/output/FeedState.kt
+++ app/src/main/java/kr/co/fastcampus/part4plus/movieapp/features/feed/presentation/output/FeedState.kt
@@ -1,9 +1,11 @@
 package kr.co.fastcampus.part4plus.movieapp.features.feed.presentation.output
 
+import kr.co.fastcampus.part4plus.movieapp.features.common.entity.MovieFeedItemEntity
+
 sealed class FeedState {
     object Loading: FeedState()
     class Main(
-        val movieList = List<MovieFeedItemEntity>
+        val movieList: List<MovieFeedItemEntity>
     ): FeedState()
 
     class Failed(
