b8938828b5d596112ff67c37d1306fa468bbcb36
915dbfl <dbfl00915@naver.com>
2024-01-27 18:25:58 +0900

    [FEAT] 필요한 detail dialog fragment들 생성

---
+++ app/src/main/java/kr/co/fastcampus/part4plus/movieapp/BaseDialogFragment.kt
@@ -0,0 +1,8 @@
+package kr.co.fastcampus.part4plus.movieapp
+
+import androidx.fragment.app.DialogFragment
+
+open class BaseDialogFragment : DialogFragment() {
+    // 매번 테마를 바꿔주기 위해 테마에 대한 설정을 넣어주기 위한 viewModel
+//    protected val themeViewModel: ThemeViewModel by activityViewModels()
+}
