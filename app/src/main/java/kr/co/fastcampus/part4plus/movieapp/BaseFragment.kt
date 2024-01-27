e7e1cdb810deb77687907adb9ec14f7f4aa60987
915dbfl <dbfl00915@naver.com>
2024-01-27 18:50:22 +0900

    [FEAT] 테마 상태 저장 data class 정의, 이를 적용한 BaseFragment/DialogFragment 생성

--- app/src/main/java/kr/co/fastcampus/part4plus/movieapp/BaseFragment.kt
+++ app/src/main/java/kr/co/fastcampus/part4plus/movieapp/BaseFragment.kt
@@ -1,10 +1,9 @@
 package kr.co.fastcampus.part4plus.movieapp
 
-import androidx.fragment.app.DialogFragment
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import kr.co.fastcampus.part4plus.movieapp.features.common.viewmodel.ThemeViewModel
 
-open class BaseDialogFragment : DialogFragment() {
-    // 매번 테마를 바꿔주기 위해 테마에 대한 설정을 넣어주기 위한 viewModel
+open class BaseFragment : Fragment() {
     protected val themeViewModel: ThemeViewModel by activityViewModels()
 }
