60d36798cfa16f5ff43ee8d04bbffba58829fe7e
915dbfl <dbfl00915@naver.com>
2024-01-27 16:04:59 +0900

    [FEAT] feed 화면 api에 대한 mapper 생성

---
+++ app/src/main/java/kr/co/fastcampus/part4plus/movieapp/features/feed/data/FeedConstants.kt
@@ -0,0 +1,7 @@
+package kr.co.fastcampus.part4plus.movieapp.features.feed.data
+
+// storage 활용 시 필요한 constants
+object FeedConstants {
+    const val MOVIE_NAME = "movieName"
+    const val MOVIE_LIST_KEY = "categories_key"
+}
